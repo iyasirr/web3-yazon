e9c69d3721565ba60671ce01c7adc7bdea55c8f7
Yasir <yasir@tcig.co>
2023-02-12 23:12:36 +0300

    First commit

---
+++ src/components/Address.js
@@ -0,0 +1,10 @@
+const Address = () => {
+    return <div style={{fontSize: 10, alignItems: 'center', justifyContent: 'center', display: 'flex',}}>
+    
+    <div style={{ backgroundColor: '#eab676', display: 'flex', justifyContent: 'center', borderRadius: 10, margin: 5, padding: 10}}>
+        <h1>TechUsed -> Hardhat, Solidity, ReactJs, EtherJs, Chai, AlChemy</h1>
+        </div>
+    </div>
+}
+
+export default Address;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => 
+<div style={{fontSize: 10, alignItems: 'center', justifyContent: 'center', display: 'flex',}}>
+    
+<div style={{ backgroundColor: '#2953DC', display: 'flex', justifyContent: 'center', borderRadius: 10, margin: 5, padding: 10}}>
+    <h1>Contract Address 0xbC78c5f74175b94799a0bc8261aE8DB5e14e26aB</h1>
+    </div>
+    <div style={{ backgroundColor: 'red', display: 'flex', justifyContent: 'center', borderRadius: 10, margin: 5, padding: 10}}>
+    <h1>Connect your wallet on Goerli Testnet</h1>
+    </div>
+</div>
+
+
+export default Header;
